9f3804864305ef826cc2f4dc320afbbb776ec049
Jules Pabst <ju.pabst@gmail.com>
2023-04-13 16:30:57 +0200

    added API

--- src/Main/CurrentData.js
+++ src/Main/CurrentData.js
@@ -1,35 +1,74 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-export default function CurrentData() {
-  const currentData = {
-    city: "New York",
-    humidity: 45,
-    wind: 4.12,
-    weatherIcon: "☁️",
-    temperature: 12,
-  };
+export default function CurrentData(props) {
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState("Paris");
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState("");
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [icon, setIcon] = useState(null);
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=8c78e9e7e9928cd1a2a6f923072c3dec`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTemperature(Math.round(data.main.temp));
+        setHumidity(data.main.humidity);
+        setWind(data.wind.speed);
+        setIcon(
+          `https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png`
+        );
+      });
+  }, []);
+
+  function submitCity(event) {}
+
+  function trackCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
   return (
-    <div className="currentData">
-      <div className="row">
-        <div className="col-7">
-          <h1 className="currentCity">{currentData.city}</h1>
-          <ul className="humidityAndWind">
-            <li>Humidty: {currentData.humidity}%</li>
-            <li>Wind: {currentData.wind} km/h</li>
-          </ul>
-        </div>
-        <div className="col-5">
-          <div>
-            <span className="weatherIcon" role="img" aria-label="cloud">
-              {currentData.weatherIcon}
-            </span>
+    <div className="weather">
+      <div className="TimeAndSearch">
+        <div className="row">
+          <div className="col-5 currentDate">Saturday 10:00</div>
+          <div className="col-7">
+            <form onSubmit={submitCity}>
+              <input
+                className="enterACity"
+                placeholder="Enter a city..."
+                type="search"
+                onChange={trackCity}
+              />
+            </form>
           </div>
-          <span className="currentTemperature">
-            {currentData.temperature}°C | °F
-          </span>
         </div>
       </div>
-      <hr />
+      <div className="currentData">
+        <div className="row">
+          <div className="col-7">
+            <h1 className="currentCity">{city}</h1>
+            <ul className="humidityAndWind">
+              <li>Humidty: {humidity}%</li>
+              <li>Wind: {wind} km/h</li>
+            </ul>
+          </div>
+          <div className="col-5">
+            <div>
+              <img src={icon} className="weatherIcon" />
+            </div>
+            <span className="currentTemperature">{temperature}°C | °F</span>
+          </div>
+        </div>
+        <hr />
+      </div>
     </div>
   );
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import TimeAndSearch from "./TimeAndSearch";
 import CurrentData from "./CurrentData";
 import Prognosis from "./Prognosis";
 
 export default function Main() {
   return (
     <div className="main">
-      <TimeAndSearch />
       <CurrentData />
       <Prognosis />
     </div>
