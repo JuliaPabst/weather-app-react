cca008c5b2fc732a4c3b1d1695639b8b48dc6e24
Jules Pabst <ju.pabst@gmail.com>
2023-04-16 19:26:45 +0200

    added weekdays

--- src/Main/Prognosis.js
+++ src/Main/Prognosis.js
@@ -1,50 +1,128 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Prognosis() {
+  const [weatherForecast, setWeatherForecast] = useState(null);
+  const [weekday, setWeekday] = useState(null);
+
+  const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  useEffect(() => {
+    let fullDate = new Date();
+    let weekdayNumber = fullDate.getDay();
+    setWeekday(weekdayNumber);
+    let currentWeekday = week[weekdayNumber];
+  });
+
+  useEffect(() => {
+    console.log("Hi");
+    const url = `https://api.shecodes.io/weather/v1/forecast?query=Obdach&key=o54b38af539a41d076df6ce6a0c0btb0`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        let weatherForecastObject = [
+          {
+            icon: data.daily[0].condition.icon_url,
+            minTemp: Math.round(data.daily[0].temperature.minimum),
+            maxTemp: Math.round(data.daily[0].temperature.maximum),
+          },
+          {
+            icon: data.daily[1].condition.icon_url,
+            minTemp: Math.round(data.daily[1].temperature.minimum),
+            maxTemp: Math.round(data.daily[1].temperature.maximum),
+          },
+          {
+            icon: data.daily[2].condition.icon_url,
+            minTemp: Math.round(data.daily[2].temperature.minimum),
+            maxTemp: Math.round(data.daily[2].temperature.maximum),
+          },
+          {
+            icon: data.daily[3].condition.icon_url,
+            minTemp: Math.round(data.daily[3].temperature.minimum),
+            maxTemp: Math.round(data.daily[3].temperature.maximum),
+          },
+          {
+            icon: data.daily[4].condition.icon_url,
+            minTemp: Math.round(data.daily[4].temperature.minimum),
+            maxTemp: Math.round(data.daily[4].temperature.maximum),
+          },
+          {
+            icon: data.daily[5].condition.icon_url,
+            minTemp: Math.round(data.daily[5].temperature.minimum),
+            maxTemp: Math.round(data.daily[5].temperature.maximum),
+          },
+        ];
+
+        setWeatherForecast(weatherForecastObject);
+      });
+  }, []);
+
   return (
     <div className="prognosis">
       <div className="row weatherDays">
         <div className="col-2">
-          Sat{" "}
-          <span role="img" aria-label="smiling face with heart-eyes">
-            ☁️
-          </span>{" "}
-          15° 11°
+          {week[weekday + 1]}
+          <img
+            src={weatherForecast[0].icon}
+            alt="weatherimage"
+            classList="miniWeatherIcon"
+            width="25"
+          />
+          {weatherForecast[0].minTemp}° {weatherForecast[0].maxTemp}°
         </div>
         <div className="col-2">
-          Sun{" "}
-          <span role="img" aria-label="smiling face with heart-eyes">
-            ☁️
-          </span>{" "}
-          15° 11°
+          {week[weekday + 2]}
+          <img
+            src={weatherForecast[1].icon}
+            alt="weatherimage"
+            classList="miniWeatherIcon"
+            width="25"
+          />
+          {weatherForecast[1].minTemp}° {weatherForecast[1].maxTemp}°
         </div>
         <div className="col-2">
-          Mon{" "}
-          <span role="img" aria-label="smiling face with heart-eyes">
-            ☁️
-          </span>{" "}
-          15° 11°
+          {week[weekday + 3]}
+          <img
+            src={weatherForecast[2].icon}
+            alt="weatherimage"
+            classList="miniWeatherIcon"
+            width="25"
+          />
+          {weatherForecast[2].minTemp}° {weatherForecast[2].maxTemp}°
         </div>
         <div className="col-2">
-          Tue{" "}
-          <span role="img" aria-label="smiling face with heart-eyes">
-            ☁️
-          </span>{" "}
-          15° 11°
+          {week[weekday + 4]}
+          <img
+            src={weatherForecast[3].icon}
+            alt="weatherimage"
+            classList="miniWeatherIcon"
+            width="25"
+          />
+          {weatherForecast[3].minTemp}° {weatherForecast[3].maxTemp}°
         </div>
         <div className="col-2">
-          Wed{" "}
-          <span role="img" aria-label="smiling face with heart-eyes">
-            ☁️
-          </span>{" "}
-          15° 11°
+          {week[weekday + 5]}
+          <img
+            src={weatherForecast[4].icon}
+            alt="weatherimage"
+            classList="miniWeatherIcon"
+            width="25"
+          />
+          {weatherForecast[4].minTemp}° {weatherForecast[4].maxTemp}°
         </div>
         <div className="col-2">
-          Wed{" "}
-          <span role="img" aria-label="smiling face with heart-eyes">
-            ☁️
-          </span>{" "}
-          15° 11°
+          {week[weekday + 6]}
+          <img
+            src={weatherForecast[5].icon}
+            alt="weatherimage"
+            classList="miniWeatherIcon"
+            width="25"
+          />
+          {weatherForecast[5].minTemp}° {weatherForecast[5].maxTemp}°
         </div>
       </div>
     </div>
